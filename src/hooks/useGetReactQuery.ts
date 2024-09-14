33250b41b0bcf19566a115dc1abbc009fdf067a2
haniejnsh <h.janeshinpour1995@gmail.com>
2024-09-14 23:30:06 +0330

    improve post subcategory

--- src/hooks/useGetReactQuery.ts
+++ src/hooks/useGetReactQuery.ts
@@ -1,8 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import axios from "../services/baseService"
 
 export default function useGetReactQuery(urlGet:string) {
-  const query= useQuery({
+  const query:UseQueryResult<any, Error>= useQuery({
     queryKey: ["product"],
     queryFn: async () => {
       const res = await axios.get(`${urlGet}`);
--- src/pages/adminPanelPage/productsManagementPage/components/AddSubCategoryForm.tsx
+++ src/pages/adminPanelPage/productsManagementPage/components/AddSubCategoryForm.tsx
@@ -1,16 +1,31 @@
 // import usePostCategory from "@/hooks/usePostReactQuery";
+import useGetReactQuery from "@/hooks/useGetReactQuery";
+import { CATEGORY_URL } from "@/services/api";
 import { Field, Form, Formik } from "formik"
 import { useRef } from "react";
 import * as Yup from "yup";
 
 
 export default function AddSubCategoryForm() {
-    // const handle=(e)=>{
-    //     e.preventDefault()
-    //     console.log("1",e);
-    //     console.log("2",e.target);
+    const {data,isLoading}=useGetReactQuery(CATEGORY_URL)
+    const categories:{catId:string;catName:string;}[]=[]
+    if(!isLoading){
+        // console.log("get data",data.data.categories);
         
-    // }
+        data.data.categories.map((cat)=>{
+        // console.log(cat.name,cat._id);
+        const catInfo:{catId:string;catName:string;}={catId:cat._id,catName:cat.name}
+        categories.push(catInfo)
+        
+        
+   })
+//    console.log("obj",categories);
+    }
+   
+   
+    // useEffect(()=>{
+
+    // })
 const formRef = useRef<HTMLFormElement>(null);
 // const {mutate}=usePostCategory()
   const handleSubmit = () => {
@@ -48,7 +63,13 @@ const formRef = useRef<HTMLFormElement>(null);
                         <span className="text-red-700 text-xs pr-4">{errors.name}</span>
                     </label>
                     <label htmlFor="category" className="flex flex-col w-full gap-1">
-                        <Field id="category" name="category" placeholder="گروه محصول" className="text-gray-500 rounded-md py-1 px-4 focus:outline-none border-[1px] border-bl2 focus:border-blue-200"/>
+                        <Field as="select" id="category" name="category" placeholder="گروه محصول" className="text-gray-500 rounded-md py-1 px-4 focus:outline-none border-[1px] border-bl2 focus:border-blue-200">
+                            {categories.map((cat) => (
+                                <option key={cat.catName} value={cat.catId}>
+                                    {cat.catName}
+                                </option>
+                            ))}
+                        </Field>
                         <span className="text-red-700 text-xs pr-4">{errors.category}</span>
                     </label>
                     <button type="submit" className="w-full mt-1 py-1 rounded-lg text-gray-500 border-1 border-[1px] border-bl2 bg-bl1 hover:bg-bl2 transition">ایجاد</button>
