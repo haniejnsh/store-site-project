bfe4ccd4ec796908a07f96046599c06b10a8187f
haniejnsh <h.janeshinpour1995@gmail.com>
2024-10-09 13:15:48 +0330

    complete unsuccessfull result page

--- src/pages/payment-result-page/SuccessfulResult.tsx
+++ src/pages/payment-result-page/SuccessfulResult.tsx
@@ -1,3 +1,5 @@
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+
 export default function SuccessfulResult() {
   return (
     <div>SuccessfulResult</div>
--- src/pages/payment-result-page/UnsuccessfulResult.tsx
+++ src/pages/payment-result-page/UnsuccessfulResult.tsx
@@ -1,5 +1,16 @@
+import { FaExclamationTriangle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
 export default function UnsuccessfulResult() {
+  const navigate =useNavigate()
   return (
-    <div>UnsuccessfulResult</div>
+    <div className="flex flex-col items-center mx-auto w-full my-16">
+      <FaExclamationTriangle className="text-9xl text-red-500"/>
+      <p className="text-red-800 mt-8 text-xl">متاسفانه پرداخت شما موفقیت آمیز نبود</p>
+      <p className="text-red-700 mt-10">سفارش شما در انتظار پرداخت میباشد</p>
+      
+      <p onClick={()=>navigate("/shipping")} className="text-blue-500 bg-bl2 px-2 py-2 rounded-lg hover:bg-blue-200 transition cursor-pointer font-bold mt-6">بازگشت به صفحه پرداخت</p>
+      
+    </div>
   )
 }
